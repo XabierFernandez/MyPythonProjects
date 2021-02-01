f966835ece66d3c3b5887d305802b1ae2639dc82
Xabier Fernandez <xabier.fernandez@outlook.com>
2021-02-02 00:23:21 +0100

    Old projects added

--- Deep_Learning/GradientDescent.py
+++ Deep_Learning/GradientDescent.py
@@ -4,12 +4,11 @@ overshoot if the input is too high
 """
 weight = 0.5
 goal_pred = 0.8
-input_1 = 0.5
-alpha = 0.1
+input_1 = 0.5 # try 2
 for iteration in range(20):
     pred = input_1 * weight
     error = (pred - goal_pred) **2
     delta = pred - goal_pred
     weight_delta = input_1 * delta
-    weight = weight - (alpha * weight_delta)
+    weight = weight - weight_delta
     print("Error:{0} Prediction:{1} Weight:{2} Iteration:{3}".format(str(error), str(pred), str(weight), str(iteration)))
