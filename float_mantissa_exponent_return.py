bfdfeae59ccc8bd23346ba51b3550622c9f156bc
Xabier Fernandez <xabier.fernandez@outlook.com>
2021-02-04 09:56:55 +0100

    fixed for any numerical base

--- float_mantissa_exponent_return.py
+++ float_mantissa_exponent_return.py
@@ -4,7 +4,7 @@ Getting sign,mantise and exponent
 import math
 
 
-def disect_float(f):
+def disect_float(f,b):
     f = float(f)  # fixes passing integers
     sign = '+'  # positive
     exp = 0
@@ -17,32 +17,26 @@ def disect_float(f):
     j = f
     if j > 0.5:
         for x in range(1072):  # exp is positive
-            res, remainder = divmod(j, 2.0)
+            res, remainder = divmod(j, b)
             #print(res, remainder,x)
             exp += 1
             if res <= 0:
                 break
-
             j = res
     else:
         for x in range(1072):  # exp is positive
-            res = j * 2.0
+            res = j * b
             exp -= 1
             #print(res,x)
             if res > 0.5:
                 break
             j = res
 
-    mant = f / pow(2, exp)
+    mant = f / pow(b, exp)
     return sign, mant, exp
 
 print("=================================================================")
-print('Sign:{0},Mantissa:{1},Exponent:{2}'.format(*disect_float(-0.000001)))
-print('The easy way: ' + str(math.frexp(-0.000001)))
+print('Sign:{0},Mantissa:{1},Exponent:{2}'.format(*disect_float(-0.0560001, 10)))
+print('The easy way: ' + str(math.frexp(-0.0560001)))
 print("=================================================================")
-print('Sign:{0},Mantissa:{1},Exponent:{2}'.format(*disect_float(-1.0034001)))
-print('The easy way: ' + str(math.frexp(-1.0034001)))
-print("=================================================================")
-print('Sign:{0},Mantissa:{1},Exponent:{2}'.format(*disect_float(231.0034001)))
-print('The easy way: ' + str(math.frexp(231.0034001)))
-print("=================================================================")
+
